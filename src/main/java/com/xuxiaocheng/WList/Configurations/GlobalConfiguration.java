47c43e3bae244ec00035d1463b2b8642f2ac9758
xuxiaocheng0201 <2762267080@qq.com>
2023-03-14 18:26:13 +0800

    feat(Config): 创建全局配置文件
    用snakeyaml创建全局配置文件，目前仅用来自定义服务端监听端口

--- src/main/java/com/xuxiaocheng/WList/Configurations/GlobalConfiguration.java
+++ src/main/java/com/xuxiaocheng/WList/Configurations/GlobalConfiguration.java
@@ -1,4 +1,44 @@
 package com.xuxiaocheng.WList.Configurations;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+
 public class GlobalConfiguration {
+    protected static @Nullable GlobalConfiguration instance;
+
+    public static @NotNull GlobalConfiguration getInstance(final @Nullable InputStream input) {
+        if (GlobalConfiguration.instance == null) {
+            if (input == null)
+                throw new IllegalArgumentException("input must be not null when initializing.");
+            GlobalConfiguration.instance = new Yaml().loadAs(input, GlobalConfiguration.class);
+            if (GlobalConfiguration.instance == null) // Empty input stream.
+                GlobalConfiguration.instance = new GlobalConfiguration();
+        }
+        return GlobalConfiguration.instance;
+    }
+
+    protected GlobalConfiguration() {
+        super();
+    }
+
+    protected int port = 5212;
+
+    public int getPort() {
+        return this.port;
+    }
+
+    @Deprecated
+    public void setPort(final int port) {
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return "GlobalConfiguration{" +
+                "port=" + this.port +
+                '}';
+    }
 }
--- src/main/java/com/xuxiaocheng/WList/WList.java
+++ src/main/java/com/xuxiaocheng/WList/WList.java
@@ -3,9 +3,13 @@ package com.xuxiaocheng.WList;
 import com.xuxiaocheng.HeadLibs.Logger.HLog;
 import com.xuxiaocheng.HeadLibs.Logger.HLogLevel;
 import com.xuxiaocheng.HeadLibs.Logger.HLoggerStream;
+import com.xuxiaocheng.WList.Configurations.GlobalConfiguration;
 import com.xuxiaocheng.WList.Internal.Server;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.InetSocketAddress;
 
 public final class WList {
@@ -19,9 +23,10 @@ public final class WList {
             WList.DebugMode ? Integer.MIN_VALUE : HLogLevel.DEBUG.getPriority() + 1,
             true, new HLoggerStream(true, !WList.DebugMode));
 
-    public static void main(final String[] args) throws InterruptedException {
+    public static void main(final String[] args) throws FileNotFoundException {
         WList.logger.log(HLogLevel.FINE, "Hello WList!");
-        final Server server = Server.getInstance(new InetSocketAddress(5212));
-        server.start().sync();
+        GlobalConfiguration.getInstance(new BufferedInputStream(new FileInputStream("config.yml")));
+        final Server server = Server.getInstance(new InetSocketAddress(GlobalConfiguration.getInstance(null).getPort()));
+        server.start().syncUninterruptibly();
     }
 }
