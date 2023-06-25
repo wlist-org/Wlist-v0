9f92b64e331e0533f92b664fcfc16909b68f2278
xuxiaocheng <2762267080@qq.com>
2023-06-25 16:47:30 +0800

    fix(Server): 提升用户密码的安全性
    提升用户密码的安全性

--- src/main/java/com/xuxiaocheng/WList/Server/Databases/User/PasswordGuard.java
+++ src/main/java/com/xuxiaocheng/WList/Server/Databases/User/PasswordGuard.java
@@ -15,7 +15,7 @@ public final class PasswordGuard {
 
     // Due to the salt being stored in the database, additional salt needs to be added to prevent password leakage.
     private static final @NotNull String ServerPasswordSlat = "WList/PasswordSlat/AdditionalSlat: " + HExceptionWrapper.wrapSupplier(() -> ConstantManager.get("PasswordSlat",
-            () -> HRandomHelper.nextString(HRandomHelper.DefaultSecureRandom, 64, ConstantManager.DefaultRandomChars), "initialize")).get();
+            () -> HRandomHelper.nextString(HRandomHelper.DefaultSecureRandom, 128, ConstantManager.DefaultRandomChars), "initialize")).get();
 
     public static @NotNull String encryptPassword(final @NotNull String password) {
         return MiscellaneousUtil.getMd5((password + PasswordGuard.ServerPasswordSlat).getBytes(StandardCharsets.UTF_8));
