4bf72005340fbe754b5078e013d639c2f5b80ca5
xuxiaocheng <2762267080@qq.com>
2023-07-12 23:34:27 +0800

    feat(Android): 添加对于Android的支持
    添加对于Android的支持(InputStream.transferTo(OutputString))

---
+++ src/main/java/com/xuxiaocheng/WListClient/Utils/AndroidSupport.java
@@ -0,0 +1,26 @@
+package com.xuxiaocheng.WListClient.Utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public final class AndroidSupport {
+    private AndroidSupport() {
+        super();
+    }
+
+    static final int DEFAULT_BUFFER_SIZE = 8192; // InputStream#DEFAULT_BUFFER_SIZE
+
+    public static void transferTo(final @NotNull InputStream inputStream, final @NotNull OutputStream outputStream) throws IOException {
+        final byte[] buffer = new byte[AndroidSupport.DEFAULT_BUFFER_SIZE];
+        int read;
+        while (true) {
+            read = inputStream.read(buffer, 0, AndroidSupport.DEFAULT_BUFFER_SIZE);
+            if (read < 0)
+                break;
+            outputStream.write(buffer, 0, read);
+        };
+    }
+}
