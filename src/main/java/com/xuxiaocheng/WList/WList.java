4d96e77ff7911ece917c8e0878c81a9b1b8216b0
xuxiaocheng <2762267080@qq.com>
2023-07-14 12:00:21 +0800

    feat(Server): 添加更多调用方支持
    添加更多启动参数设置
    添加对Android的启动阶段API支持

--- src/main/java/com/xuxiaocheng/WList/WList.java
+++ src/main/java/com/xuxiaocheng/WList/WList.java
@@ -22,30 +22,53 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class WList {
     private WList() {
         super();
     }
 
-    public static final boolean DebugMode = true;
-    public static final boolean InIdeaMode = !new File(WList.class.getProtectionDomain().getCodeSource().getLocation().getPath()).isFile();
-    static {
-        if (!WList.InIdeaMode && System.getProperty("HLogLevel.color") == null) System.setProperty("HLogLevel.color", "2");
-        HLog.setDebugMode(WList.DebugMode);
+    private static boolean DebugMode = true;
+
+    public static boolean isDebugMode() {
+        return WList.DebugMode;
     }
 
-    private static final HLog logger = HLog.createInstance("DefaultLogger",
-            WList.InIdeaMode ? Integer.MIN_VALUE : WList.DebugMode ? HLogLevel.LESS.getLevel() : HLogLevel.FINE.getLevel(),
-            true,  WList.InIdeaMode ? null : HMergedStream.getFileOutputStreamNoException(null));
-
-    public static void main(final String @NotNull [] args) throws IOException, SQLException, InterruptedException {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> WList.logger.log(HLogLevel.FAULT, "Uncaught exception. thread: ", t.getName(), e));
-        if (WList.DebugMode)
+    public static final boolean InIdeaMode = false;//!new File(WList.class.getProtectionDomain().getCodeSource().getLocation().getPath()).isFile();
+    static {
+        if (!WList.InIdeaMode && System.getProperty("HLogLevel.color") == null)
+            System.setProperty("HLogLevel.color", "2");
+        if (WList.isDebugMode())
             System.setProperty("io.netty.leakDetectionLevel", "ADVANCED");
+    }
+
+    private static final HLog logger = WList.InIdeaMode ? HLog.createInstance("DefaultLogger", Integer.MIN_VALUE, false, true) :
+            HLog.createInstance("DefaultLogger", WList.isDebugMode() ? Integer.MIN_VALUE : HLogLevel.FINE.getLevel(), false, true, HMergedStream.getFileOutputStreamNoException(null));
+
+    private static final @NotNull AtomicInteger mainStageAPI = new AtomicInteger(-1);
+
+    public static int getMainStageAPI() {
+        return WList.mainStageAPI.get();
+    }
+
+    public static void main(final String @NotNull ... args) throws IOException, SQLException, InterruptedException {
+        if (!WList.mainStageAPI.compareAndSet(-1, 0))
+            return;
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> WList.logger.log(HLogLevel.FAULT, "Uncaught exception. thread: ", t.getName(), e));
+        File configuration = new File("server.yaml");
+        for (final String arg: args) {
+            if ("-Debug".equalsIgnoreCase(arg))
+                WList.DebugMode = true;
+            if ("-NoDebug".equalsIgnoreCase(arg))
+                WList.DebugMode = false;
+            if (arg.startsWith("-path:"))
+                configuration = new File(arg.substring("-file:".length()), "server.yaml");
+        }
+        configuration = configuration.getAbsoluteFile();
+        HLog.setDebugMode(WList.DebugMode);
         WList.logger.log(HLogLevel.FINE, "Hello WList! Loading...");
-        final File configuration = new File(args.length > 0 ? args[0] : "server.yaml");
-        WList.logger.log(HLogLevel.LESS, "Initializing global configuration. file: ", configuration.getAbsolutePath());
+        WList.logger.log(HLogLevel.LESS, "Initializing global configuration. file: ", configuration);
         GlobalConfiguration.initialize(configuration);
         ConstantManager.initialize();
         WList.logger.log(HLogLevel.VERBOSE, "Initialized global configuration.");
@@ -54,18 +77,21 @@ public final class WList {
         UserManager.initialize();
         WList.logger.log(HLogLevel.VERBOSE, "Initialized user database.");
         WList.logger.log(HLogLevel.LESS, "Initializing driver manager.");
-        DriverManager.initialize();
+        DriverManager.initialize(new File(configuration.getParentFile(), "configs"));
         WList.logger.log(HLogLevel.VERBOSE, "Initialized driver manager.");
         try {
-            WList.logger.log(HLogLevel.VERBOSE, "Initializing WList server.");
+            WList.logger.log(HLogLevel.LESS, "Initializing WList server.");
             ServerHandlerManager.initialize();
-            WListServer.init(new InetSocketAddress(GlobalConfiguration.getInstance().port()));
+            WListServer.initialize(new InetSocketAddress(GlobalConfiguration.getInstance().port()));
             WList.logger.log(HLogLevel.VERBOSE, "Initialized WList server.");
+            WList.mainStageAPI.set(1);
             WListServer.getInstance().start();
             WListServer.getInstance().awaitStop();
         } finally {
+            WList.mainStageAPI.set(2);
             WList.logger.log(HLogLevel.FINE, "Shutting down the whole application...");
             if (GlobalConfiguration.getInstance().dumpConfiguration()) {
+                // TODO Save in time.
                 WList.logger.log(HLogLevel.INFO, "Saving driver configurations in multithreading...");
                 for (final Pair.ImmutablePair<WebDriversType, DriverInterface<?>> driver: DriverManager.getAll().values())
                     WListServer.ServerExecutors.submit(HExceptionWrapper.wrapRunnable(() -> DriverManager.dumpConfiguration(driver.getSecond().getConfiguration())));
@@ -76,8 +102,9 @@ public final class WList {
             futures[2] = WListServer.IOExecutors.shutdownGracefully();
             futures[3] = BackgroundTaskManager.BackgroundExecutors.shutdownGracefully();
             for (final Future<?> future: futures)
-                future.syncUninterruptibly();
+                future.sync();
             WList.logger.log(HLogLevel.MISTAKE, "Thanks to use WList.");
+            WList.mainStageAPI.set(3);
         }
     }
 }
--- src/main/java/com/xuxiaocheng/WList/WListTest.java
+++ src/main/java/com/xuxiaocheng/WList/WListTest.java
@@ -1,15 +1,19 @@
 package com.xuxiaocheng.WList;
 
+import com.xuxiaocheng.HeadLibs.DataStructures.Pair;
+import com.xuxiaocheng.HeadLibs.Functions.HExceptionWrapper;
 import com.xuxiaocheng.HeadLibs.Functions.SupplierE;
 import com.xuxiaocheng.HeadLibs.Logger.HLog;
 import com.xuxiaocheng.HeadLibs.Logger.HLogLevel;
-import com.xuxiaocheng.WList.Driver.DriverInterface;
 import com.xuxiaocheng.WList.Databases.Constant.ConstantManager;
 import com.xuxiaocheng.WList.Databases.User.UserManager;
 import com.xuxiaocheng.WList.Databases.UserGroup.UserGroupManager;
+import com.xuxiaocheng.WList.Driver.DriverInterface;
+import com.xuxiaocheng.WList.Server.Driver.BackgroundTaskManager;
 import com.xuxiaocheng.WList.Server.Driver.DriverManager;
 import com.xuxiaocheng.WList.Server.GlobalConfiguration;
 import com.xuxiaocheng.WList.Server.WListServer;
+import com.xuxiaocheng.WList.WebDrivers.WebDriversType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,8 +28,10 @@ public final class WListTest {
     public static void main(final String @NotNull [] args) throws Exception {
 //        if (true) return;
         WListTest.wrapServerInitialize(() -> {
-            final DriverInterface<?> driver = Objects.requireNonNull(DriverManager.get("123pan"));
-
+            final DriverInterface<?> driver = Objects.requireNonNull(DriverManager.get("123pan_136"));
+//            HLog.DefaultLogger.log("",
+//                    DriverManager_123pan.getFileInformation((DriverConfiguration_123Pan) driver.getConfiguration(), 2345490, null, null)
+//            );
 //            TrashedFileManager.initialize(configuration.getName());
 //            return TrashManager_123pan.restoreFile(configuration, 2293734, true, null, null);
             return null;
@@ -34,22 +40,26 @@ public final class WListTest {
 
 
     @SuppressWarnings("OverlyBroadThrowsClause")
-    static void wrapServerInitialize(final @NotNull SupplierE<@Nullable Object> runnable) throws Exception {
+    private static void wrapServerInitialize(final @NotNull SupplierE<@Nullable Object> runnable) throws Exception {
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> HLog.DefaultLogger.log(HLogLevel.FAULT, "Uncaught exception. thread: ", t.getName(), e));
         System.setProperty("io.netty.leakDetectionLevel", "ADVANCED");
         GlobalConfiguration.initialize(new File("server.yaml"));
         ConstantManager.initialize();
         UserGroupManager.initialize();
         UserManager.initialize();
-        DriverManager.initialize();
+        DriverManager.initialize(new File("configs"));
         try {
             final Object obj = runnable.get();
             if (obj != null)
                 HLog.DefaultLogger.log("", obj);
         } finally {
-            WListServer.CodecExecutors.shutdownGracefully().syncUninterruptibly();
-            WListServer.ServerExecutors.shutdownGracefully().syncUninterruptibly();
-            WListServer.IOExecutors.shutdownGracefully().syncUninterruptibly();
+            if (GlobalConfiguration.getInstance().dumpConfiguration())
+                for (final Pair.ImmutablePair<WebDriversType, DriverInterface<?>> driver: DriverManager.getAll().values())
+                    WListServer.ServerExecutors.submit(HExceptionWrapper.wrapRunnable(() -> DriverManager.dumpConfiguration(driver.getSecond().getConfiguration())));
+            WListServer.CodecExecutors.shutdownGracefully();
+            WListServer.ServerExecutors.shutdownGracefully();
+            WListServer.IOExecutors.shutdownGracefully();
+            BackgroundTaskManager.BackgroundExecutors.shutdownGracefully();
         }
     }
 }
